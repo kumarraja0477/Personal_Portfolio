98d762f01563fbf2a60db8a751495bfa8c8284db
KUMAR RAJA <rajakhushi.8@gmail.com>
2023-10-08 18:44:51 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get tabLinks & tabContents
+var tabLinks = document.getElementsByClassName("tab-links"),
+tabContents = document.getElementsByClassName("tab-contents");
+
+// On openTab function call
+function openTab (tabname) {
+for (item of tabLinks) {
+item.classList.remove("active-link");
+} 
+for (item of tabContents) { 
+item.classList.remove("active-tab");
+}
+event.currentTarget.classList.add("active-link");
+document.getElementById(tabname).classList.add("active-tab");
+}
